8aecb776e0163042aa59c8fcecb44c82b73bcc53
Christoph Jung <jagodki.cj@gmail.com>
2018-10-18 06:16:47 +0200

    update data view

---
+++ src/Lunis/Lunis/iPhoneGUI/DataViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DataViewController.swift
+//  Lunis
+//
+//  Created by Christoph on 18.10.18.
+//  Copyright © 2018 jagodki. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        //add a search bar to the navigation bar
+        let searchController = UISearchController(searchResultsController: nil)
+        self.navigationItem.searchController = searchController
+        self.navigationItem.hidesSearchBarWhenScrolling = true
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
