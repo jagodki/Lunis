f7344b5b63529b0a9defd86019ee3e322b676cc3
Christoph Jung <jagodki.cj@gmail.com>
2018-11-29 20:07:48 +0100

    start work with coredata

--- src/Lunis/Lunis/iPhoneGUI/AdditionalViews/DownloadStory/DownloadTableViewController.swift
+++ src/Lunis/Lunis/iPhoneGUI/AdditionalViews/DownloadStory/DownloadTableViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import CoreData
+
+struct DownloadSection {
+    var title: String
+    var rows: [DownloadRow]
+}
+
+struct DownloadRow {
+    var name: String
+}
 
 class DownloadTableViewController: UITableViewController {
     
-    // MARK: - Outlets
+    // MARK: - Table Data
+    var tableData = [
+        DownloadSection(title: "Sverige", rows: [
+            DownloadRow(name: "Stockholm"),
+            DownloadRow(name: "Malmö")
+            ]),
+        DownloadSection(title: "Polska", rows: [
+            DownloadRow(name: "Warszawa")
+            ]),
+        DownloadSection(title: "Deutschland", rows: [
+            DownloadRow(name: "Radebeul"),
+            DownloadRow(name: "Meißen")
+            ])
+    ]
     
     override func viewDidLoad() {
+        //add a search bar to the navigation bar
+        let searchController = UISearchController(searchResultsController: nil)
+        super.navigationItem.searchController = searchController
+        super.navigationItem.hidesSearchBarWhenScrolling = true
+        
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
@@ -30,59 +58,36 @@ class DownloadTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return self.tableData.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.tableData[section].rows.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "downloadTableCell", for: indexPath)
+        cell.textLabel?.text = self.tableData[indexPath.section].rows[indexPath.row].name
+        
+        //change the colour to gray, if the dataset is already downloaded
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.tableData[section].title
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //download the selected dataset
+        
+        //deselect the current row
+        self.tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
