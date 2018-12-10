70368ac7facd4aba53d966ca9af8b2c043e1d8d6
Christoph Jung <jagodki.cj@gmail.com>
2018-12-10 20:30:52 +0100

    rearrange download view in IB

--- src/Lunis/Lunis/iPhoneGUI/DownloadViewController.swift
+++ src/Lunis/Lunis/iPhoneGUI/DownloadViewController.swift
@@ -7,20 +7,51 @@
 //
 
 import UIKit
+import MapKit
 
-class DownloadViewController: UIViewController {
+struct DownloadSection {
+    var title: String
+    var rows: [DownloadRow]
+}
+
+struct DownloadRow {
+    var name: String
+}
+
+class DownloadViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: - Table Data
+    var tableData = [
+        DownloadSection(title: "Sverige", rows: [
+            DownloadRow(name: "Stockholm"),
+            DownloadRow(name: "Malmö")
+        ]),
+        DownloadSection(title: "Polska", rows: [
+            DownloadRow(name: "Warszawa")
+        ]),
+        DownloadSection(title: "Deutschland", rows: [
+            DownloadRow(name: "Radebeul"),
+            DownloadRow(name: "Meißen")
+        ])
+    ]
     
     // MARK: - Outlets
     @IBOutlet var segmentedControlContainer: UISegmentedControl!
-    @IBOutlet var containerList: UIView!
-    @IBOutlet var containerMap: UIView!
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //add a search bar to the navigation bar
+        let searchController = UISearchController(searchResultsController: nil)
+        super.navigationItem.searchController = searchController
+        super.navigationItem.hidesSearchBarWhenScrolling = true
+        
         self.segmentedControlContainer.selectedSegmentIndex = 0
-        self.containerList.alpha = 1
-        self.containerMap.alpha = 0
+        self.tableView.alpha = 1
+        self.mapView.alpha = 0
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,18 +59,53 @@ class DownloadViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - IBActions
     @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             UIView.animate(withDuration: 0.5, animations: {
-                self.containerList.alpha = 1
-                self.containerMap.alpha = 0
+                self.tableView.alpha = 1
+                self.mapView.alpha = 0
             })
         } else {
             UIView.animate(withDuration: 0.5, animations: {
-                self.containerList.alpha = 0
-                self.containerMap.alpha = 1
+                self.tableView.alpha = 0
+                self.mapView.alpha = 1
+                //self.navigationItem.searchController.is
             })
         }
+    }
+    
+    // MARK: - TableView implementation
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableData[section].rows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "downloadTableCell", for: indexPath)
+        cell.textLabel?.text = self.tableData[indexPath.section].rows[indexPath.row].name
+        
+        //mark datasets, that are already downloaded
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.tableData[section].title
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //download the selected dataset
+        
+        //deselect the current row
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.tableData.count
     }
     
 }
