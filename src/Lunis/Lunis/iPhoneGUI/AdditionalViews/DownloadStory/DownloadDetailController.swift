02798ba43e43fbcf3050c66b1bf02747c78de878
Christoph Jung <jagodki.cj@gmail.com>
2019-04-15 19:44:17 +0200

    change barbuttonitem in download detail view

--- src/Lunis/Lunis/iPhoneGUI/AdditionalViews/DownloadStory/DownloadDetailController.swift
+++ src/Lunis/Lunis/iPhoneGUI/AdditionalViews/DownloadStory/DownloadDetailController.swift
@@ -25,6 +25,7 @@ class DownloadDetailController: UITableViewController {
     
     // MARK: - instance variables
     var administration: Administration!
+    var tableData: [DownloadDetailRow] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,9 +36,16 @@ class DownloadDetailController: UITableViewController {
         let rowCity = DownloadDetailRow(title: "City", value: self.administration.city!)
         let rowRegion = DownloadDetailRow(title: "Region", value: self.administration.region!)
         let rowCountry = DownloadDetailRow(title: "Country", value: self.administration.country!)
-        let rowCount = DownloadDetailRow(title: "Schools", value: String(self.administration.schools.count))
+        let rowCount = DownloadDetailRow(title: "Schools", value: String(self.administration.schools!.count))
         let rowSize = DownloadDetailRow(title: "Size", value: "xx MB")
-        let group = DownloadDetailRow(title: "", rows: [rowCity, rowRegion, rowCountry, rowCount, rowSize])
+        self.tableData = [rowCity, rowRegion, rowCountry, rowCount, rowSize]
+        
+        //edit the right barbutton
+        if self.datasetIsAlreadyOnDevice() {
+            self.buttonSaveDelete = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(self.deleteData(sender:)))
+        } else {
+            self.buttonSaveDelete = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(self.saveData(sender:)))
+        }
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -59,58 +67,29 @@ class DownloadDetailController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.tableData.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "downloadDetailCell", for: indexPath)
+        cell.textLabel?.text = self.tableData[indexPath.row].title
+        cell.detailTextLabel?.text = self.tableData[indexPath.row].value
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+    
+    private func datasetIsAlreadyOnDevice() -> Bool {
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    @objc private func saveData(sender: UIBarButtonItem) {
+        
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    @objc private func deleteData(sender: UIBarButtonItem) {
+        
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 
 }
